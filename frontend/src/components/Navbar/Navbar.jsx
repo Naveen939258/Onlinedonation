dbb5e45184033bae26a232c5800db07077780ce8
Kakarla Naveen <2200031998@kluniversity.in>
2025-11-07 15:12:36 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { faBell, faHeart, faUserCircle } from "@fortawesome/free-solid-svg-icons
 import axios from "axios";
 import "./Navbar.css";
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+const API_BASE = process.env.REACT_APP_API_URL || "https://onlinedonation.onrender.com/api";
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
